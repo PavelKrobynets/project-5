e977843c15266d4bdf5089d758253dbd7a1e302e
Pavel <krobynetspavel@gmail.com>
2023-10-04 14:22:46 +0300

    fixing bugs

--- src/js/script.js
+++ src/js/script.js
@@ -49,5 +49,38 @@ $(document).ready(function(){
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
     })
-});
+    });
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста введите свое имя",
+                    minlength: jQuery.validator.format("Минимум {0} символа в строке")
+                },
+                phone: "Пожалуйста введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста введите свой мейл",
+                    email: "Мейл должен быть офрмата name@domain.com"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#order form');
+    validateForms('#consultation form');
+
+    $('input[name=phone]').mask("+3 (999) 999-99-99");
 });
